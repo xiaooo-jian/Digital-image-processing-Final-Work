24ef15e3fce6826908a11163208d9c9ff4d3c3e3
AlexanderJian <1164195534@qq.com>
2020-06-15 17:46:48 +0800

    sixth commit-- 进一步完善图像界面的业务逻辑

--- GUI.py
+++ GUI.py
@@ -1,33 +1,73 @@
 import sys
 from PyQt5.QtWidgets import (QWidget, QPushButton,
-                             QHBoxLayout, QVBoxLayout, QApplication)
+                             QHBoxLayout, QVBoxLayout, QApplication, QDesktopWidget, QLabel, QLineEdit, QFormLayout)
+from qtpy import QtCore
 
 
 class Example(QWidget):
 
     def __init__(self):
+        self.card = {"王": 2, "2": 4, "A": 4, "K": 4, "Q": 4, "J": 4, "10": 4, "9": 4, "8": 4, "7": 4, "6": 4, "5": 4,
+                     "4": 4, "3": 4}
         super().__init__()
 
         self.initUI()
 
-    def initUI(self):
-        okButton = QPushButton("开始")
-        cancelButton = QPushButton("截取")
+    def center(self):
+        # 获得窗口
+        qr = self.frameGeometry()
+        # 获得屏幕中心点
+        cp = QDesktopWidget().availableGeometry().center()
+        # 显示到屏幕中心
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(okButton)
-        hbox.addWidget(cancelButton)
+    def initUI(self):
+        start = QPushButton("开始")
+        screen = QPushButton("截取")
+
+        start.clicked.connect(self.start)
+        screen.clicked.connect(self.screenshot)
+
+        button_box = QHBoxLayout()
+        button_box.addStretch(1)
+        button_box.addWidget(start)
+        button_box.addWidget(screen)
 
         vbox = QVBoxLayout()
+        card = QLabel("牌型： 王 | 2 | A | K | Q | J | 10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 ")
+        number = QLabel(self.getcard())
+
+        vbox.addWidget(card)
+        vbox.addWidget(number)
         vbox.addStretch(1)
-        vbox.addLayout(hbox)
+        vbox.addLayout(button_box)
 
         self.setLayout(vbox)
+        self.resize(570, 100)
+        self.setWindowTitle('记牌器')
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
 
-        self.setGeometry(300, 300, 500, 150)
-        self.setWindowTitle('Buttons')
+        self.center()
         self.show()
+
+    def start(self):
+        self.card = {"王": 2, "2": 4, "A": 4, "K": 4, "Q": 4, "J": 4, "10": 4, "9": 4, "8": 4, "7": 4, "6": 4, "5": 4,
+                     "4": 4, "3": 4}
+
+        print("start")
+
+    def screenshot(self):
+        self.card = {"王": 2, "2": 2, "A": 4, "K": 4, "Q": 4, "J": 4, "10": 4, "9": 4, "8": 4, "7": 4, "6": 4, "5": 4,
+                     "4": 4, "3": 2}
+        QApplication.processEvents()
+        print("screen")
+
+    def getcard(self):
+        return "数量：  {} | {} | {} | {} | {} | {} |  {} | {} | {} | {} | {} | {} | {} | {} ".format(
+            self.card['王'], self.card["2"], self.card["A"], self.card["K"], self.card["Q"], self.card["J"],
+            self.card["10"],self.card["9"], self.card["8"], self.card["7"], self.card["6"], self.card["5"],
+            self.card["4"], self.card["3"])
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import ImagePartition as ip
 import ImageCapture as ic
 
 
-while not ip.getMasterCard(ic.screenShot()):
-    print("游戏尚未开始")
 
 
 
